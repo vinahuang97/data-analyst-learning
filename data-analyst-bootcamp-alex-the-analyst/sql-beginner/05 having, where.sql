8f4fcf170366f154366ef016940d5c3d40521c92
Ervina Huang <144708018+vinahuang97@users.noreply.github.com>
2024-10-12 15:19:05 +0700

    sql-beginner

---
+++ data-analyst-bootcamp-alex-the-analyst/sql-beginner/05 having, where.sql
@@ -0,0 +1,15 @@
+-- having and where
+
+select gender, avg(age)
+from employee_demographics
+group by gender
+having avg(age) >40
+;
+
+select occupation, avg(salary)
+from employee_salary
+where occupation like '%manager%'
+group by occupation
+having avg(salary) >75000
+;
+
